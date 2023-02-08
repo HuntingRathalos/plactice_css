1272e830c7fad69bdb7983b8bc1252849c666f46
HuntingRathalos <huntingandrathalos@gmail.com>
2023-02-08 17:44:25 +0900

    タイトル、サブタイトル実装 #1

--- front-dev-tutorial-master/98_サンプルサイト_練習用/scripts/main.js
+++ front-dev-tutorial-master/98_サンプルサイト_練習用/scripts/main.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hero = new HeroSlider(".swiper");
     hero.start();
-});
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const hero = new HeroSlider('.swiper');
-//     hero.start();
-// });
+    // const ta = new TweenTextAnimation(".tween-animate-title");
+    // ta.animate();
+
+    const cb = function (el, isIntersecting) {
+        if (isIntersecting) {
+            const ta = new TweenTextAnimation(el);
+            ta.animate();
+        }
+    };
+
+    const so = new ScrollObserver(".tween-animate-title", cb);
+});
